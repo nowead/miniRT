0a86120bbd3b3ddef4f9ffd100622698f24a2eb4
seonseo <seonseo@student.42seoul.kr>
2024-09-18 00:26:16 +0900

    fix get_normal_vector.c

--- sources/render/get_normal_vector.c
+++ sources/render/get_normal_vector.c
@@ -6,7 +6,7 @@
 /*   By: seonseo <seonseo@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 15:47:24 by seonseo           #+#    #+#             */
-/*   Updated: 2024/09/16 20:25:04 by seonseo          ###   ########.fr       */
+/*   Updated: 2024/09/18 00:25:31 by seonseo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,20 +29,22 @@ t_vec3	get_normal_vector(t_point3 p, t_closest_hit *hit)
 
 t_vec3	get_cylinder_normal(t_point3 p, t_closest_hit *hit)
 {
-	t_vec3	temp;
-	t_vec3	q;
-	float	m;
+	t_cylinder	*cylinder;
+	t_vec3		temp;
+	t_vec3		q;
+	float		m;
 
+	cylinder = &hit->obj->data.cylinder;
 	if (hit->sub_obj == TOP_CAP)
-		return (hit->obj->data.cylinder.axis);
+		return (cylinder->top_cap.normal);
 	else if (hit->sub_obj == BOTTOM_CAP)
-		return (scale_vector(hit->obj->data.cylinder.axis, -1));
+		return (cylinder->bottom_cap.normal);
 	else
 	{
-		temp = subtract_3dpoints(p, hit->obj->data.cylinder.center);
-        m = dot(temp, hit->obj->data.cylinder.axis);
-        q = add_vector_to_point(hit->obj->data.cylinder.center,\
-		 scale_vector(hit->obj->data.cylinder.axis, m));
+		temp = subtract_3dpoints(p, hit->obj->data.cylinder.side.center);
+        m = dot(temp, hit->obj->data.cylinder.side.axis);
+        q = add_vector_to_point(hit->obj->data.cylinder.side.center,\
+		 scale_vector(hit->obj->data.cylinder.side.axis, m));
         return (unit_vector(subtract_3dpoints(p, q)));
 	}
 }
@@ -52,12 +54,13 @@ t_vec3	get_cone_normal(t_point3 p, t_closest_hit *hit)
 	t_cone	*cone;
 	t_vec3	vp;
 
+	cone = &hit->obj->data.cone;
 	if (hit->sub_obj == BOTTOM_CAP)
-		return (hit->obj->data.cone.axis);
+		return (hit->obj->data.cone.bottom_cap.normal);
 	else
 	{
 		cone = &hit->obj->data.cone;
-		vp = subtract_3dpoints(p, cone->vertex);
-		return (unit_vector(cross(vp, cross(vp, cone->axis))));
+		vp = subtract_3dpoints(p, cone->side.vertex);
+		return (unit_vector(cross(vp, cross(vp, cone->side.axis))));
 	}
 }
